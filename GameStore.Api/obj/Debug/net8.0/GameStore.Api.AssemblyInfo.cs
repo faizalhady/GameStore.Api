d1aced7d9e914ed3d6099d389707ff65e9d915fc
faizalhady <syedfaizalhady@gmail.com>
2024-10-11 00:35:49 +0800

    added games http

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c1d92313c339440039db41191b703348d4c3f1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
